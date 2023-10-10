68df72569ef1d3019bc18704707ab5c40550228e
IvetteMo <tp984@correo.tij.ibero.mx>
2023-10-10 15:16:19 -0700

    Agregar archivos de proyecto.

---
+++ HMIbasik/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace HMIbasik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pgbConnection = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pbLedImages = new System.Windows.Forms.PictureBox();
+            this.btnOff = new System.Windows.Forms.Button();
+            this.btnOn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.cmbBaud = new System.Windows.Forms.ComboBox();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLedImages)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pgbConnection
+            // 
+            this.pgbConnection.Location = new System.Drawing.Point(13, 255);
+            this.pgbConnection.Name = "pgbConnection";
+            this.pgbConnection.Size = new System.Drawing.Size(465, 50);
+            this.pgbConnection.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pbLedImages);
+            this.groupBox2.Controls.Add(this.btnOff);
+            this.groupBox2.Controls.Add(this.btnOn);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox2.Location = new System.Drawing.Point(496, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(402, 293);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "LED Control";
+            // 
+            // pbLedImages
+            // 
+            this.pbLedImages.Image = global::HMIbasik.Properties.Resources.green_led_on;
+            this.pbLedImages.Location = new System.Drawing.Point(107, 29);
+            this.pbLedImages.Name = "pbLedImages";
+            this.pbLedImages.Size = new System.Drawing.Size(212, 195);
+            this.pbLedImages.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLedImages.TabIndex = 6;
+            this.pbLedImages.TabStop = false;
+            // 
+            // btnOff
+            // 
+            this.btnOff.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOff.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnOff.Location = new System.Drawing.Point(229, 243);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(113, 44);
+            this.btnOff.TabIndex = 5;
+            this.btnOff.Text = "LED off";
+            this.btnOff.UseVisualStyleBackColor = true;
+            this.btnOff.Click += new System.EventHandler(this.btnOff_Click);
+            // 
+            // btnOn
+            // 
+            this.btnOn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnOn.Location = new System.Drawing.Point(49, 243);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(113, 44);
+            this.btnOn.TabIndex = 4;
+            this.btnOn.Text = "LED On";
+            this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnClose);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Controls.Add(this.cmbBaud);
+            this.groupBox1.Controls.Add(this.cmbPort);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(465, 220);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COM Port Settings";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(29, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "BAUD RATE:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(29, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COM PORT:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnClose.Location = new System.Drawing.Point(264, 160);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(113, 44);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnOpen.Location = new System.Drawing.Point(65, 160);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(113, 44);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // cmbBaud
+            // 
+            this.cmbBaud.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbBaud.FormattingEnabled = true;
+            this.cmbBaud.Location = new System.Drawing.Point(205, 99);
+            this.cmbBaud.Name = "cmbBaud";
+            this.cmbBaud.Size = new System.Drawing.Size(236, 37);
+            this.cmbBaud.TabIndex = 1;
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(205, 40);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(236, 37);
+            this.cmbPort.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(915, 317);
+            this.Controls.Add(this.pgbConnection);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Control LED";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLedImages)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pgbConnection;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pbLedImages;
+        private System.Windows.Forms.Button btnOff;
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox cmbBaud;
+        private System.Windows.Forms.ComboBox cmbPort;
+    }
+}
+
---
+++ HMIbasik/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HMIbasik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //El único botón habilitado al momento de ejecutar el formulario
+            //Es el de Open
+            btnOpen.Enabled = true;
+            btnClose.Enabled = false;
+            btnOn.Enabled = false;
+            btnOff.Enabled = false;
+
+            //Se establecen valores por defecto para el combobox
+            //EL progressbar comienza en 0 y el picturebox con la
+            //imagen del Led Apagado
+            cmbBaud.Text = "9600";
+            pgbConnection.Value = 0;
+            pbLedImages.Image = Properties.Resources.green_led_off;
+
+            //Se crea una lista de los puertos disponibles
+            string[] portList = SerialPort.GetPortNames();
+            cmbPort.Items.AddRange(portList);
+
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            //Estructura try/ catch para el manejo de excepciones
+            try
+            {
+                //Obteniendo el nombre del puerto del combobox y //convirtiendo a dato numérico el valor del baud rate
+                //Abiendo el puerto serial
+                serialPort1.PortName = cmbPort.Text;
+                serialPort1.BaudRate = Convert.ToInt32(cmbBaud.Text);
+                serialPort1.Open();
+
+                //Se deshabilita el botón open
+                //Se carga el progress bar al 100%
+                btnOpen.Enabled = false;
+                btnClose.Enabled = true;
+                btnOn.Enabled = true;
+                btnOff.Enabled = true;
+                pgbConnection.Value = 100;
+                //velocidad de transmisión de datos en una comunicación serie.
+                cmbBaud.Enabled = false;
+                cmbPort.Enabled = false;
+
+            }
+            catch (Exception error)
+            {
+                //Si sucede algun error se despliega un mensaje 
+                MessageBox.Show(error.Message);
+            }
+
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //Condicional para determinar si el puerto está abierto
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    //Escribir en el puerto serial  Off y colocar la imagen
+                    //De led apagado
+                    serialPort1.WriteLine("$Off");
+                    pbLedImages.Image = Properties.Resources.green_led_off;
+
+                    //Cerrar el puerto
+                    serialPort1.Close();
+
+                    //Habilitar el botón de open y deshabilitar los demás
+                    btnOpen.Enabled = true;
+                    btnClose.Enabled = false;
+                    btnOn.Enabled = false;
+                    btnOff.Enabled = false;
+                    pgbConnection.Value = 0;
+                    cmbBaud.Enabled = true;
+                    cmbPort.Enabled = true;
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+
+        }
+
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            //Si se da clic en el boton On:
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    //Colocar la imagen del LED encendido y escribir en el
+                    //Puerto serial On
+                    pbLedImages.Image = Properties.Resources.green_led_on;
+
+                    serialPort1.WriteLine("$On");
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+
+
+        }
+
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+
+
+            //Si se da clic en el boton apagar:
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    //Cargar la imagen de led apagago y escribir en el puerto
+                    //serial Off
+                    pbLedImages.Image = Properties.Resources.green_led_off;
+
+                    serialPort1.WriteLine("$Off");
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+
+
+        }
+    }
+}
